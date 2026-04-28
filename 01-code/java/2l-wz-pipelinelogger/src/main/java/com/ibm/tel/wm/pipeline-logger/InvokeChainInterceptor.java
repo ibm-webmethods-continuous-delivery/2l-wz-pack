3c4d47708216ee6f83d803f576b1972ea22aa567
miunibm <175016327+miunibm@users.noreply.github.com>
2026-04-28 15:21:21 +0000

    Seed for pipeline logger interceptor
    Signed-off-by: miunibm <175016327+miunibm@users.noreply.github.com>

---
+++ 01-code/java/2l-wz-pipelinelogger/src/main/java/com/ibm/tel/wm/pipeline-logger/InvokeChainInterceptor.java
@@ -0,0 +1,45 @@
+package com.ibm.tel.wm.pipelinelogger;
+import com.wm.app.b2b.server.BaseService;
+import com.wm.app.b2b.server.invoke.InvokeChainProcessor;
+import com.wm.app.b2b.server.invoke.InvokeManager;
+import com.wm.app.b2b.server.invoke.ServiceStatus;
+import com.wm.data.IData;
+import com.wm.util.JournalLogger;
+import com.wm.util.ServerException;
+import java.util.Iterator;
+
+public class InvokeChainInterceptor implements InvokeChainProcessor {
+	public static final InvokeChainInterceptor INSTANCE = new InvokeChainInterceptor();
+
+	static {
+    InvokeManager.getDefault().registerProcessor(INSTANCE);
+
+    JournalLogger.logInfo(JournalLogger.LOG_EXCEPTION, JournalLogger.FAC_LICENSE_MGR, "Expert Labs License monitor processor registered");
+	}
+	
+	public InvokeChainInterceptor() {
+	}
+
+	@Override
+	public void process(@SuppressWarnings("rawtypes") Iterator chain
+                      , BaseService svc, IData pipeline, ServiceStatus status) 
+                      throws ServerException {
+    if (!status.isTopService()) {
+			if (chain.hasNext()) {
+				((InvokeChainProcessor) chain.next()).process(chain, svc, pipeline, status);
+			}
+      return; // If not a top level service, just pass the ball down the chain
+    }
+
+    long startTime = System.currentTimeMillis();
+		String serviceNS = svc.getNSName().getFullName();
+
+    try {
+      if (chain.hasNext()) {
+        ((InvokeChainProcessor) chain.next()).process(chain, svc, pipeline, status);
+      }
+    } finally {
+      long duration = System.currentTimeMillis() - startTime;
+    }
+	}
+}
