bd06e62d99f1ef576839d830ca0c60453ae0ffaa
miunibm <175016327+miunibm@users.noreply.github.com>
2026-05-08 13:28:59 +0000

    ZzMftConfig with certificates and ports from json. Still WIP.
    Signed-off-by: miunibm <175016327+miunibm@users.noreply.github.com>

---
+++ 01-code/is-packages/ZzMftConfig/code/source/zz/config/mft/util.java
@@ -0,0 +1,90 @@
+package zz.config.mft;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.wm.app.b2b.server.PackageManager;
+import com.wm.app.b2b.server.cluster.CMException;
+import com.wm.app.b2b.server.cluster.ClusterManager;
+import com.wm.util.EncUtil;
+import com.wm.app.log.impl.sc.LevelTranslator;
+import com.wm.lang.flow.IDataWmPathProcessor;
+import com.wm.util.JournalLogger;
+import com.entrust.toolkit.util.ByteArray;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+import java.lang.reflect.*;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.util.*;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class util
+
+{
+	// ---( internal utility methods )---
+
+	final static util _instance = new util();
+
+	static util _newInstance() { return new util(); }
+
+	static util _cast(Object o) { return (util)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void readFileAsBytes (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(readFileAsBytes)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required filename
+		// [o] object:0:required fileBytes
+		// pipeline
+		IDataCursor pipelineCursor = pipeline.getCursor();
+			String	filename = IDataUtil.getString( pipelineCursor, "filename" );
+		pipelineCursor.destroy();
+		
+		byte[] array = null;
+		try {
+			array = Files.readAllBytes(Paths.get(filename));
+		} catch (IOException e) {
+			throw new ServiceException(e);
+		}			
+		
+		// pipeline
+		IDataCursor pipelineCursor_1 = pipeline.getCursor();
+		IDataUtil.put( pipelineCursor_1, "fileBytes", array );
+		pipelineCursor_1.destroy();
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
